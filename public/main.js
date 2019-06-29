5af9a7f76eef987122489d31bb071d0282557ceb
Rhys Bray <rhysabray@hotmail.com>
2019-06-29 18:31:41 +0100

    main functionality built. question diversity is low due to only '+' operator being used. also added media query for styling when phone's keyboard is on the screen for use

---
+++ public/main.js
@@ -0,0 +1,48 @@
+let questionsCorrect = 0;
+let questionsAnswered = 0;
+let randomNumber1 = 0;
+let randomNumber2 = 0;
+let correctAnswer = 0;
+
+const initialiseGame = () => {
+  questionsCorrect = 0;
+  questionsAnswered = 0;
+  newRound();
+};
+
+newRound = () => {
+  document.querySelector("input").value = "";
+  document.getElementById("score").innerHTML =
+    "Score: " + questionsCorrect + "/" + questionsAnswered;
+  newQuestion();
+};
+
+const newQuestion = () => {
+  randomNumber1 = randomNumber();
+  randomNumber2 = randomNumber();
+  correctAnswer = randomNumber1 + randomNumber2;
+  //   randomOperator needed
+  document.getElementById("question").innerHTML =
+    randomNumber1 + " + " + randomNumber2 + "?";
+};
+
+randomNumber = () => {
+  return Math.floor(Math.random() * 10) + 1;
+};
+
+submitAnswer = () => {
+  let title = document.getElementById("title");
+  let answer = document.querySelector("input").value;
+  if (answer == correctAnswer) {
+    questionsCorrect += 1;
+    title.innerHTML = "That Was Correct!";
+    title.classList = "correct";
+  } else {
+    title.innerHTML = "Incorrect..";
+    title.classList = "wrong";
+  }
+  questionsAnswered += 1;
+  newRound();
+};
+
+initialiseGame();
